246e929ead849b40e173df7129fc872300b52488
Journeyman-dev <60055347+Journeyman-dev@users.noreply.github.com>
2021-07-03 11:27:07 -0400

    add cursor.h and cursor.c for cursor management functions

---
+++ include/TUIC/cursor.h
@@ -0,0 +1,40 @@
+/*
+	Copyright (c) 2021 Daniel Valcour
+	
+	Permission is hereby granted, free of charge, to any person obtaining a copy of
+	this software and associated documentation files (the "Software"), to deal in
+	the Software without restriction, including without limitation the rights to
+	use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+	the Software, and to permit persons to whom the Software is furnished to do so,
+	subject to the following conditions:
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+	
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+	FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+	COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+	IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+	CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+/*! \file cursor.h
+ */
+ #ifndef TUIC_CURSOR_H //header guard
+#define TUIC_CURSOR_H
+#ifdef __cplusplus //extern C guard
+extern "C" {
+#endif
+#include <TUIC/types.h>
+
+TuiCursor tuiCreateCursor(TuiImage image, int xhot, int yhot);
+
+TuiCursor tuiCreateStandardCursor(int shape);
+
+void tuiDestroyCursor(TuiCursor cursor);
+
+int tuiGetCursorCount();
+
+#ifdef __cplusplus //extern C guard
+}
+#endif
+#endif //header guard
---
+++ src/cursor.c
@@ -0,0 +1,62 @@
+#include <TUIC/cursor.h>
+#include "image_inline.h"
+#include <GLFW/glfw3.h>
+#include <TUIC/debug.h>
+#include <TUIC/system.h>
+#include <TUIC/boolean.h>
+
+static size_t sCursorCount;
+
+TuiCursor tuiCreateCursor(TuiImage image, int xhot, int yhot)
+{
+	if (tuiIsActive() == TUI_FALSE)
+	{
+		// TODO tuiDebugError(TUI_ERROR_INACTIVE_SYSTEM, __func__);
+		return;
+	}
+	if (image == NULL)
+	{
+		tuiDebugError(TUI_ERROR_NULL_IMAGE, __func__);
+		return NULL;
+	}
+
+	GLFWimage glfw_image = _TuiImageToGlfwImage(image, __func__);
+	if (glfw_image.width = 0)
+	{
+		return NULL;
+	}
+	sCursorCount++;
+	return glfwCreateCursor(&glfw_image, xhot, yhot);
+}
+
+TuiCursor tuiCreateStandardCursor(int shape)
+{
+	if (tuiIsActive() == TUI_FALSE)
+	{
+		// TODO tuiDebugError(TUI_ERROR_INACTIVE_SYSTEM, __func__);
+		return;
+	}
+	// TODO if (tuiCursorShapeIsValid(shape) == TUI_FALSE)
+	{
+		// TODO tuiDebugError(TUI_ERROR_INVALID_CURSOR_SHAPE, __func__);
+		return NULL;
+	}
+	sCursorCount++;
+	return glfwCreateStandardCursor(shape);
+}
+
+void tuiDestroyCursor(TuiCursor cursor)
+{
+	if (cursor == NULL)
+	{
+		// TODO tuiDebugError(TUI_ERROR_NULL_CURSOR, __func__);
+		return;
+	}
+	sCursorCount--;
+	glfwDestroyCursor(cursor);
+}
+
+int tuiGetCursorCount()
+{
+	return (int)sCursorCount;
+}
