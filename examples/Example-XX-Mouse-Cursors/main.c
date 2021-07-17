05154d93ceb9f3ce7e33a8c4e271516cdb68421f
Journeyman-dev <60055347+Journeyman-dev@users.noreply.github.com>
2021-07-17 10:44:16 -0400

    modify cursor example to use key callback

--- examples/Example-XX-Mouse-Cursors/main.c
+++ examples/Example-XX-Mouse-Cursors/main.c
@@ -15,6 +15,10 @@ void TuiMessageCallback(TuiErrorCode error_code, const char* msg)
     }
 }
 
+static TuiImage custom_cursor_image = NULL;
+static TuiCursorShape current_cursor_shape = TUI_CURSOR_SHAPE_FIRST;
+static TuiCursor current_cursor = NULL;
+
 TuiCursorShape increment_cursor_shape(TuiCursorShape current_shape)
 {
     if (current_shape != 0 && current_shape < TUI_CURSOR_SHAPE_LAST)
@@ -39,6 +43,28 @@ TuiCursorShape increment_cursor_shape(TuiCursorShape current_shape)
     }
 }
 
+void key_callback(TuiWindow window, TuiKey key, int scancode, TuiButtonState button_state, TuiKeyMod key_mod)
+{
+    if (key == TUIK_C && button_state == TUI_BUTTON_PRESS)
+    {
+        current_cursor_shape = increment_cursor_shape(current_cursor_shape);
+        if (current_cursor_shape == 0)
+        {
+            TuiCursor next_cursor = tuiCursorCreate(custom_cursor_image, 8, 8);
+            tuiWindowSetCursor(window, next_cursor);
+            tuiCursorDestroy(current_cursor);
+            current_cursor = next_cursor;
+        }
+        else
+        {
+            TuiCursor next_cursor = tuiCursorCreateStandard(current_cursor_shape);
+            tuiWindowSetCursor(window, next_cursor);
+            tuiCursorDestroy(current_cursor);
+            current_cursor = next_cursor;
+        }
+    }
+}
+
 int main()
 {
     int tiles_wide = 50;
@@ -60,39 +86,25 @@ int main()
     /* Tell TUIC to send all debug messages to our message callback (the function we implemented above main). */
     tuiSetDebugErrorCallback(TuiMessageCallback);
 
+
+
+    /* Load the custom cursor image. */
+    custom_cursor_image  = tuiImageLoad("custom_cursor.png", 0);
+
+    /* Initialize the current cursor. */
+    current_cursor = tuiCursorCreateStandard(current_cursor_shape);
+
     /* Create the window. */
     const char* window_title = "Example XX";
     TuiWindow window = tuiWindowCreate(window_width, window_height, window_title, NULL);
 
-    /* Load the custom cursor image. */
-    TuiImage custom_cursor_image = tuiImageLoad("custom_cursor.png", 0);
-
-    TuiCursorShape current_cursor_shape = TUI_CURSOR_SHAPE_FIRST;
-    TuiCursor current_cursor = tuiCursorCreateStandard(current_cursor_shape);
+    /* Set the window key callback. */
+    tuiWindowSetKeyboardKeyCallback(window, key_callback);
 
     //Render loop
     while (tuiWindowShouldClose(window) == TUI_FALSE)
     {
-        tuiPollEvents(); //handle input events and call callback functions
-
-        if (tuiWindowGetKey(window, TUIK_C) == TUI_BUTTON_PRESS)
-        {
-            current_cursor_shape = increment_cursor_shape(current_cursor_shape);
-            if (current_cursor_shape == 0)
-            {
-                TuiCursor next_cursor = tuiCursorCreate(custom_cursor_image, 8, 8);
-                tuiWindowSetCursor(window, next_cursor);
-                tuiCursorDestroy(current_cursor);
-                current_cursor = next_cursor;
-            }
-            else
-            {
-                TuiCursor next_cursor = tuiCursorCreateStandard(current_cursor_shape);
-                tuiWindowSetCursor(window, next_cursor);
-                tuiCursorDestroy(current_cursor);
-                current_cursor = next_cursor;
-            }
-        }
+        tuiPollEvents(); //handle input events and call callback function.
 
         tuiWindowSwapBuffers(window); //swap the window buffers
     }
