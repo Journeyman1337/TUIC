4cc178b599ff8a344d663a54aa72e85ccb37fb1a
Journeyman-dev <60055347+Journeyman-dev@users.noreply.github.com>
2021-07-30 16:17:42 -0400

    check if TUIC is initialized in event functions

--- src/events.c
+++ src/events.c
@@ -2,9 +2,17 @@
 #include <GLFW/glfw3.h>
 #include <TUIC/debug.h>
 #include "glfw_error_check.h"
+#include "objects.h"
 
 void tuiPollEvents()
 {
+	TuiSystem system = tui_get_system();
+	if (system == NULL)
+	{
+		tuiDebugError(TUI_ERROR_NOT_INITIALIZED, __func__);
+		return;
+	}
+
 	glfwPollEvents();
 	TuiErrorCode glfw_error = _GlfwErrorCheck();
 	if (glfw_error != TUI_ERROR_NONE)
@@ -15,6 +23,13 @@ void tuiPollEvents()
 
 void tuiWaitEvents()
 {
+	TuiSystem system = tui_get_system();
+	if (system == NULL)
+	{
+		tuiDebugError(TUI_ERROR_NOT_INITIALIZED, __func__);
+		return;
+	}
+
 	glfwWaitEvents();
 	TuiErrorCode glfw_error = _GlfwErrorCheck();
 	if (glfw_error != TUI_ERROR_NONE)
@@ -25,6 +40,12 @@ void tuiWaitEvents()
 
 void tuiWaitEventsTimeout(double timeout)
 {
+	TuiSystem system = tui_get_system();
+	if (system == NULL)
+	{
+		tuiDebugError(TUI_ERROR_NOT_INITIALIZED, __func__);
+		return;
+	}
 	if (timeout < 0.0 || 
 		timeout == 0x7ff0000000000000ULL ||// INFINITY
 		 timeout == 0x7ff0000000000001ULL // NaN
@@ -44,6 +65,13 @@ void tuiWaitEventsTimeout(double timeout)
 
 void tuiPostEmptyEvent()
 {
+	TuiSystem system = tui_get_system();
+	if (system == NULL)
+	{
+		tuiDebugError(TUI_ERROR_NOT_INITIALIZED, __func__);
+		return;
+	}
+
 	glfwPostEmptyEvent();
 	TuiErrorCode glfw_error = _GlfwErrorCheck();
 	if (glfw_error != TUI_ERROR_NONE)
