ad9057c89b310b8bd1e445d3095c4828ab9e6c2d
Journeyman-dev <60055347+Journeyman-dev@users.noreply.github.com>
2021-08-06 14:38:59 -0400

    spacing window.h

--- include/TUIC/window.h
+++ include/TUIC/window.h
@@ -1,6 +1,6 @@
 /*
 	Copyright (c) 2021 Daniel Valcour
-	
+
 	Permission is hereby granted, free of charge, to any person obtaining a copy of
 	this software and associated documentation files (the "Software"), to deal in
 	the Software without restriction, including without limitation the rights to
@@ -9,7 +9,7 @@
 	subject to the following conditions:
 	The above copyright notice and this permission notice shall be included in all
 	copies or substantial portions of the Software.
-	
+
 	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
 	FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
@@ -125,7 +125,7 @@ TuiWindowCreateInfo tuiWindowCreateInfo();
  * Functions for manipulating @ref TuiWindow opaque objects.
  *  @{ */
 /*!
- * @brief Create a new @ref TuiWindow. 
+ * @brief Create a new @ref TuiWindow.
  *
  * @param framebuffer_pixel_width The pixel width of the window's framebuffer.
  * @param framebuffer_pixel_height The pixel height of the window's framebuffer.
