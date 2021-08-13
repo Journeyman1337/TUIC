7b60ce7c326e9fa0f18ea878739012a240dbde3e
Journeyman-dev <60055347+Journeyman-dev@users.noreply.github.com>
2021-08-13 17:28:07 -0400

    fix raw mouse motion test
    remove mouse motion mode changing on TUI_BUTTON_REPEAT
    fix raw mouse motion enable/disable func names
    add close with escape key
    use TUI_CURSOR_MODE_DISABLED

--- test/manual/raw_mouse_motion.cpp
+++ test/manual/raw_mouse_motion.cpp
@@ -6,10 +6,10 @@
 
 void key_callback(TuiWindow window, TuiKeyboardKey key, int scancode, TuiButtonState state, TuiKeyboardMod mods)
 {
-	if (key == TUIK_SPACE && (  state == TUI_BUTTON_PRESS || state == TUI_BUTTON_REPEAT))
+	if (key == TUIK_SPACE && state == TUI_BUTTON_PRESS)
 	{
-		TuiBoolean raw_mouse_motion_enabled = tuiGetRawMouseMotionEnabled(window);
-		tuiSetRawMouseMotionEnabled(!raw_mouse_motion_enabled);
+		TuiBoolean raw_mouse_motion_enabled = tuiWindowGetRawMouseMotionEnabled(window);
+		tuiWindowSetRawMouseMotionEnabled(window, !raw_mouse_motion_enabled);
 		if (raw_mouse_motion_enabled == TUI_TRUE)
 		{
 			printf("Raw mouse motion disabled.\n");
@@ -18,6 +18,10 @@ void key_callback(TuiWindow window, TuiKeyboardKey key, int scancode, TuiButtonS
 		{
 			printf("Raw mouse motion enabled.\n");
 		}
+	}
+	if (key == TUIK_ESCAPE && state == TUI_BUTTON_PRESS)
+	{
+		tuiWindowSetShouldClose(window, TUI_TRUE);
 	}
 }
 
@@ -56,14 +60,17 @@ int main()
 	
 	TuiWindow window = tuiWindowCreate(256, 256, "raw_mouse_motion", TUI_NULL);
 	tuiWindowSetKeyboardKeyCallback(window, key_callback);
-	tuiWindodwSetCursorMoveCallback(window, cursor_move);
+	tuiWindowSetCursorMoveCallback(window, cursor_move);
+
+	tuiWindowSetCursorMode(window, TUI_CURSOR_MODE_DISABLED);
 	
-	printf("Press spacebar to enable or disable raw mouse motion.\nRaw mouse motion disabled.\n");
+	printf("Press spacebar to enable or disable raw mouse motion. Press escape to close the window.\nRaw mouse motion disabled.\n");
 	
-	while(tuiWindowShouldClose(window) == TUI_FALSE)
+	while (tuiWindowShouldClose(window) == TUI_FALSE)
 	{
 		tuiPollEvents();
 		tuiWindowFrame(window);
+	}
 	
 	tuiWindowDestroy(window);
 	window = TUI_NULL;
