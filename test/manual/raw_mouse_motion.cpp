1904e66f238f925658f6476aef82d8f5834ed5f0
Journeyman-dev <60055347+Journeyman-dev@users.noreply.github.com>
2021-08-13 17:06:39 -0400

    add raw mouse motion test

--- test/manual/raw_mouse_motion.cpp
+++ test/manual/raw_mouse_motion.cpp
@@ -1,1 +1,74 @@
 /* Test raw mouse motion. */
+
+#include <TUIC/tuic.h>
+
+#include <stdio.h>
+
+void key_callback(TuiWindow window, TuiKeyboardKey key, int scancode, TuiButtonState state, TuiKeyboardMod mods)
+{
+	if (key == TUIK_SPACE && (  state == TUI_BUTTON_PRESS || state == TUI_BUTTON_REPEAT))
+	{
+		TuiBoolean raw_mouse_motion_enabled = tuiGetRawMouseMotionEnabled(window);
+		tuiSetRawMouseMotionEnabled(!raw_mouse_motion_enabled);
+		if (raw_mouse_motion_enabled == TUI_TRUE)
+		{
+			printf("Raw mouse motion disabled.\n");
+		}
+		else //if (raw_mouse_motion_enabled == TUI_FALSE)
+		{
+			printf("Raw mouse motion enabled.\n");
+		}
+	}
+}
+
+void cursor_move(TuiWindow window, double x_position, double y_position)
+{
+	printf("Cursor move: ( x = %f, y = %f )\n", x_position, y_position);
+}
+
+void message_callback(TuiErrorCode error_code, const char* msg)
+{
+	if (error_code == TUI_ERROR_GRAPHICS_BACKEND_SPECIFIC)
+	{
+		printf("%s : %s backend message: %s\n", tuiErrorCodeToString(error_code), tuiErrorCodeGetDescription(error_code), msg);
+	}
+	else
+	{
+		printf("%s : %s location: %s\n", tuiErrorCodeToString(error_code), tuiErrorCodeGetDescription(error_code), msg);
+	}
+}
+
+int main()
+{
+	tuiSetDebugErrorCallback(message_callback);
+	
+	if (tuiInit() == TUI_FALSE)
+	{
+		printf("Failed to initialize TUIC.");
+		return 1;
+	}
+	
+	if (tuiRawMouseMotionSupported() == TUI_FALSE)
+	{
+		printf("Raw mouse motion is not supported.");
+		return 0;
+	}
+	
+	TuiWindow window = tuiWindowCreate(256, 256, "raw_mouse_motion", TUI_NULL);
+	tuiWindowSetKeyboardKeyCallback(window, key_callback);
+	tuiWindodwSetCursorMoveCallback(window, cursor_move);
+	
+	printf("Press spacebar to enable or disable raw mouse motion.\nRaw mouse motion disabled.\n");
+	
+	while(tuiWindowShouldClose(window) == TUI_FALSE)
+	{
+		tuiPollEvents();
+		tuiWindowFrame(window);
+	
+	tuiWindowDestroy(window);
+	window = TUI_NULL;
+	
+	tuiTerminate();	
+	
+	return 0;
+}
