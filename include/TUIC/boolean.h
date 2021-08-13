dd6a33dbf654418f650322f96e3cd9b478279908
Journeyman-dev <60055347+Journeyman-dev@users.noreply.github.com>
2021-08-13 17:26:38 -0400

    make const boolean values static
    prevent linking errors due to the values existing in other translation units

--- include/TUIC/boolean.h
+++ include/TUIC/boolean.h
@@ -35,11 +35,11 @@ typedef uint8_t TuiBoolean;
 /*!
  * @brief Boolean false value.
  */
-const TuiBoolean TUI_TRUE = 1;
+static const TuiBoolean TUI_TRUE = 1;
 /*!
  * @brief Boolean false value.
  */
-const TuiBoolean TUI_FALSE = 0;
+static const TuiBoolean TUI_FALSE = 0;
 
 
 #ifdef __cplusplus //extern C guard
