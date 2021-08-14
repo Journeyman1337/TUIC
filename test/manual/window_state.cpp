b43950c40dd28dd366198fc5526ca028d8ed853b
Journeyman-dev <60055347+Journeyman-dev@users.noreply.github.com>
2021-08-14 18:21:56 -0400

    fix window_state test
    include tuic and stdio.h
    fix invalid functions
    add a second window for handling input focus that is topmost

--- test/manual/window_state.cpp
+++ test/manual/window_state.cpp
@@ -1,46 +1,65 @@
 /* Test window state changes including: iconify, maximize, restore, hide, show, focus, fullscreen, and windowed. */
 
+#include <TUIC/tuic.h>
+
+#include <stdio.h>
+
 void key_callback(TuiWindow window, TuiKeyboardKey key, int scancode, TuiButtonState state, TuiKeyboardMod mods)
 {
+	TuiWindow state_window = (TuiWindow)tuiWindowGetUserPointer(window);
 	if (key == TUIK_I && state == TUI_BUTTON_PRESS)
 	{
-		TuiBoolean iconified = tuiWindowGetIconified(window);
-		tuiWindowSetIconified(window, !iconified);
+		TuiBoolean iconified = tuiWindowGetIconified(state_window);
+		if (iconified == TUI_FALSE)
+		{
+			tuiWindowIconify(state_window);
+		}
+		else //if (iconified == TUI_TRUE)
+		{
+			tuiWindowRestore(state_window);
+		}
 	}
 	if (key == TUIK_M && state == TUI_BUTTON_PRESS)
 	{
-		TuiBoolean maximized = tuiWindowGetMaximized(window);
-		tuiWindowSetMaximized(window, !maximized);
+		TuiBoolean maximized = tuiWindowGetMaximized(state_window);
+		if (maximized == TUI_TRUE)
+		{
+			tuiWindowRestore(state_window);
+		}
+		else //if (maximized == TUI_FALSE)
+		{
+			tuiWindowMaximize(state_window);
+		}
 	}
 	if (key == TUIK_R && state == TUI_BUTTON_PRESS)
 	{
-		tuiWindowRestore(window);
+		tuiWindowRestore(state_window);
 	}
 	if (key == TUIK_H && state == TUI_BUTTON_PRESS)
 	{
-		TuiBoolean visible = tuiWindowGetVisible(window);
+		TuiBoolean visible = tuiWindowGetVisible(state_window);
 		if (visible == TUI_TRUE)
 		{
-			tuiWindowHide(window);
+			tuiWindowHide(state_window);
 			printf("Window hidden.\n");
 		}
 		else
 		{
-			tuiWindowShow(window);
+			tuiWindowShow(state_window);
 			printf("Window shown.\n");
 		}
 	}
 	if (key == TUIK_F && state == TUI_BUTTON_PRESS)
 	{
-		TuiBoolean fullscreen = tuiWindowGetFullscreen(window);
-		if (fullscreen == TUI_TRUE)
+		TuiBoolean fullscreen = tuiWindowGetIsFullscreen(state_window);
+		if (fullscreen == TUI_FALSE)
 		{
-			tuiWindowSetFullscreenCurrentMonitor(window);
+			tuiWindowSetFullscreen(state_window, tuiGetPrimaryMonitor());
 			printf("Window made fullscreen.\n");
 		}
-		else
+		else //if (fullscreen == TUI_TRUE)
 		{
-			tuiWindowSetWindowed(window);
+			tuiWindowSetWindowedViewportSize(window, 256, 256);
 			printf("Window made windowed.\n");
 		}
 	}
@@ -92,20 +111,30 @@ int main()
 		return 1;
 	}
 	
-	TuiWindow window = tuiWindowCreate(256, 256, "window_state", TUI_NULL);
-	tuiWindowSetKeyboardKeyCallback(window, key_callback);
+	TuiWindow window = tuiWindowCreate(256, 256, "State Changing Window", TUI_NULL);
+	
 	tuiWindowSetIconifyCallback(window, iconify_callback);
 	tuiWindowSetMaximizeCallback(window, maximize_callback);
+
+	TuiWindow input_focus_window = tuiWindowCreate(256, 256, "Input Focus Window", TUI_NULL);
+	tuiWindowSetUserPointer(input_focus_window, window);
+	tuiWindowSetKeyboardKeyCallback(input_focus_window, key_callback);
+	tuiWindowSetTopmost(input_focus_window, TUI_TRUE);
+
+	printf("Give the input focus window input focus to change the state of the other window. Press M to maximize and restore the window. Press I to iconify the window. Press F to toggle between fullscreen and windowed. Press H to hide the window.\n");
 	
-	while (tuiWindowShouldClose(window) == TUI_FALSE)
+	while (tuiWindowShouldClose(window) == TUI_FALSE && tuiWindowShouldClose(input_focus_window) == TUI_FALSE)
 	{
 		tuiPollEvents();
 		tuiWindowClearColor(window, 255, 0, 0, 255);
 		tuiWindowFrame(window);
+		tuiWindowFrame(input_focus_window);
 	}
 	
 	tuiWindowDestroy(window);
 	window = TUI_NULL;
+	tuiWindowDestroy(input_focus_window);
+	input_focus_window = TUI_NULL;
 	
 	tuiTerminate();
 	
