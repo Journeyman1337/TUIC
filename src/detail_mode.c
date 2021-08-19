d92e047d7a377ae21e433e1fa5169b2d5ab0516c
Journeyman-dev <60055347+Journeyman-dev@users.noreply.github.com>
2021-08-19 15:17:18 -0400

    make detail modes with G0 glyph flag only compatible with TUI_BLEND_NORMAL

--- src/detail_mode.c
+++ src/detail_mode.c
@@ -360,7 +360,7 @@ TuiBoolean tuiModesAreCompatible(TuiDetailMode detail_mode, TuiBlendMode blend_m
 {
 	if (blend_mode == TUI_BLEND_NORMAL)
 	{
-		if (tuiDetailHasFlag(detail_mode, TUI_DETAIL_FLAG_COLOR_0) == TUI_TRUE)
+		if (tuiDetailHasFlag(detail_mode, TUI_DETAIL_FLAG_COLOR_0) == TUI_TRUE || tuiDetailHasFlag(detail_mode, TUI_DETAIL_FLAG_GLYPH_0) == TUI_TRUE)
 		{
 			return TUI_TRUE;
 		}
@@ -369,7 +369,7 @@ TuiBoolean tuiModesAreCompatible(TuiDetailMode detail_mode, TuiBlendMode blend_m
 			return TUI_FALSE;
 		}
 	}
-	else if (tuiDetailHasFlag(detail_mode, TUI_DETAIL_FLAG_COLOR_0) == TUI_TRUE)
+	else if (tuiDetailHasFlag(detail_mode, TUI_DETAIL_FLAG_COLOR_0) == TUI_TRUE || tuiDetailHasFlag(detail_mode, TUI_DETAIL_FLAG_GLYPH_0) == TUI_TRUE)
 	{
 		return TUI_FALSE;
 	}
