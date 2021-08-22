6b5e44292378ccb97a2b7a7c9f1d067a80fb088b
Journeyman-dev <60055347+Journeyman-dev@users.noreply.github.com>
2021-08-22 09:59:24 -0400

    remove the DrawBatchData functions for TuiWindow and TuiPanel
    batch data has gotten a lot more complicated this update, and so this type of function would be very confusing. its less error prone for people to use the TuiBatch struct  to handle the data for them, so remove support for batch data

--- src/panel.c
+++ src/panel.c
@@ -332,52 +332,6 @@ void tuiPanelDrawBatch(TuiPanel panel, TuiAtlas atlas, TuiPalette palette, TuiBa
 	}
 }
 
-void tuiPanelDrawBatchData(TuiPanel panel, TuiAtlas atlas, TuiPalette palette, TuiDetailMode detail_mode, int tiles_wide, int tiles_tall, size_t sparse_index, const uint8_t* batch_data)
-{
-	TuiSystem system = tui_get_system();
-	if (system == TUI_NULL)
-	{
-		tuiDebugError(TUI_ERROR_NOT_INITIALIZED, __func__);
-		return;
-	}
-	if (panel == TUI_NULL)
-	{
-		tuiDebugError(TUI_ERROR_NULL_PANEL, __func__);
-		return;
-	}
-	if (atlas == TUI_NULL)
-	{
-		tuiDebugError(TUI_ERROR_NULL_ATLAS, __func__);
-		return;
-	}
-	if (batch_data == TUI_NULL)
-	{
-		tuiDebugError(TUI_ERROR_NULL_BATCH_DATA, __func__);
-		return;
-	}
-	if (tiles_wide <= 0 || tiles_tall <= 0)
-	{
-		tuiDebugError(TUI_ERROR_INVALID_BATCH_DATA_DIMENSIONS, __func__);
-		return;
-	}
-	if (tuiDetailHasPalette(detail_mode) == TUI_TRUE && palette == TUI_NULL)
-	{
-		tuiDebugError(TUI_ERROR_PALETTE_REQUIRED, __func__);
-		return;
-	}
-	if (tuiDetailHasFlag(detail_mode, TUI_DETAIL_FLAG_LAYOUT_SPARSE) == TUI_TRUE && sparse_index == 0)
-	{
-		return;
-	}
-
-	TuiErrorCode error_code = tuiPanelDrawBatchData_Opengl33(panel, atlas, palette, (size_t)detail_mode, (size_t)tiles_wide, (size_t)tiles_tall, sparse_index, batch_data, 0, panel->PixelWidth, 0, panel->PixelHeight);
-	if (error_code != TUI_ERROR_NONE)
-	{
-		tuiDebugError(error_code, __func__);
-		return;
-	}
-}
-
 void tuiPanelDrawBatchTransformed(TuiPanel panel, TuiAtlas atlas, TuiPalette palette, TuiBatch batch, int left_x, int right_x, int top_y, int bottom_y)
 {
 	TuiSystem system = tui_get_system();
@@ -412,52 +366,6 @@ void tuiPanelDrawBatchTransformed(TuiPanel panel, TuiAtlas atlas, TuiPalette pal
 	}
 
 	TuiErrorCode error_code = tuiPanelDrawBatchData_Opengl33(panel, atlas, palette, batch->DetailMode, batch->TilesWide, batch->TilesTall, batch->TileCount, batch->Data, left_x, right_x, top_y, bottom_y);
-	if (error_code != TUI_ERROR_NONE)
-	{
-		tuiDebugError(error_code, __func__);
-		return;
-	}
-}
-
-void tuiPanelDrawBatchDataTransformed(TuiPanel panel, TuiAtlas atlas, TuiPalette palette, TuiDetailMode detail_mode, int tiles_wide, int tiles_tall, size_t sparse_index, const uint8_t* batch_data, int left_x, int right_x, int top_y, int bottom_y)
-{
-	TuiSystem system = tui_get_system();
-	if (system == TUI_NULL)
-	{
-		tuiDebugError(TUI_ERROR_NOT_INITIALIZED, __func__);
-		return;
-	}
-	if (panel == TUI_NULL)
-	{
-		tuiDebugError(TUI_ERROR_NULL_PANEL, __func__);
-		return;
-	}
-	if (atlas == TUI_NULL)
-	{
-		tuiDebugError(TUI_ERROR_NULL_ATLAS, __func__);
-		return;
-	}
-	if (batch_data == TUI_NULL)
-	{
-		tuiDebugError(TUI_ERROR_NULL_BATCH_DATA, __func__);
-		return;
-	}
-	if (tiles_wide <= 0 || tiles_tall <= 0)
-	{
-		tuiDebugError(TUI_ERROR_INVALID_PANEL_DIMENSIONS, __func__);
-		return;
-	}
-	if (tuiDetailHasPalette(detail_mode) == TUI_TRUE && palette == TUI_NULL)
-	{
-		tuiDebugError(TUI_ERROR_PALETTE_REQUIRED, __func__);
-		return;
-	}
-	if (tuiDetailHasFlag(detail_mode, TUI_DETAIL_FLAG_LAYOUT_SPARSE) == TUI_TRUE && sparse_index == 0)
-	{
-		return;
-	}
-
-	TuiErrorCode error_code = tuiPanelDrawBatchData_Opengl33(panel, atlas, palette, (size_t)detail_mode, (size_t)tiles_wide, (size_t)tiles_tall, sparse_index, batch_data, left_x, right_x, top_y, bottom_y);
 	if (error_code != TUI_ERROR_NONE)
 	{
 		tuiDebugError(error_code, __func__);
