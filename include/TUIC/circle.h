15754c665c114347ea137bacbb372ee481dc4da0
Journeyman-dev <60055347+Journeyman-dev@users.noreply.github.com>
2021-09-11 17:03:55 -0400

    declare an implement TuiCircle functions

--- include/TUIC/circle.h
+++ include/TUIC/circle.h
@@ -28,7 +28,216 @@ extern "C" {
 #include <TUIC/boolean.h>
 
 
-
+/*! @name 2D Circle Functions
+ *
+ * Functions for manipulating @ref TuiCircle.
+ *  @{ */
+/*!
+ * @brief Create a @ref TuiCircle.
+ * 
+ * @param center_x The x position of one of the circle center point.
+ * @param center_y The y position of one of the circle center point.
+ * @param radius The radius of the circle.
+ *
+ * @returns The created @ref TuiCircle.
+ *
+ * @errors This function can have no errors.
+ *
+ * @requirements This function can be called freely, even if TUIC is not currently initialized.
+ *
+ * @thread_safety This function does not access memory in ways that could cause a data race, so it is thread safe.
+ */
+TuiCircle tuiCircle(const int center_x, const int center_y, const float radius);
+/*!
+ * @brief Get the center @ref TuiPoint2 of a @ref TuiCircle.
+ *
+ * @param circle The @ref TuiCircle.
+ *
+ * @returns The center @ref TuiPoint2
+ *
+ * @errors This function can have no error codes.
+ *
+ * @requirements This function can be called freely, even if TUIC is not currently initialized.
+ *
+ * @thread_safety This function does not access memory in ways that could cause a data race, so it is thread safe.
+ */
+TuiPoint2 tuiCircleGetCenterPoint2(const TuiCircle circle);
+/*!
+ * @brief Get the topmost @ref TuiPoint2 of a @ref TuiCircle.
+ *
+ * @param circle The @ref TuiCircle.
+ *
+ * @returns The topmost @ref TuiPoint2
+ *
+ * @errors This function can have no error codes.
+ *
+ * @requirements This function can be called freely, even if TUIC is not currently initialized.
+ *
+ * @thread_safety This function does not access memory in ways that could cause a data race, so it is thread safe.
+ */
+TuiPoint2 tuiCircleGetTopPoint2(const TuiCircle circle);
+/*!
+ * @brief Get the leftmost @ref TuiPoint2 of a @ref TuiCircle.
+ *
+ * @param circle The @ref TuiCircle.
+ *
+ * @returns The leftmost @ref TuiPoint2
+ *
+ * @errors This function can have no error codes.
+ *
+ * @requirements This function can be called freely, even if TUIC is not currently initialized.
+ *
+ * @thread_safety This function does not access memory in ways that could cause a data race, so it is thread safe.
+ */
+TuiPoint2 tuiCircleGetLeftPoint2(const TuiCircle circle);
+/*!
+ * @brief Get the bottomost @ref TuiPoint2 of a @ref TuiCircle.
+ *
+ * @param circle The @ref TuiCircle.
+ *
+ * @returns The bottomost @ref TuiPoint2
+ *
+ * @errors This function can have no error codes.
+ *
+ * @requirements This function can be called freely, even if TUIC is not currently initialized.
+ *
+ * @thread_safety This function does not access memory in ways that could cause a data race, so it is thread safe.
+ */
+TuiPoint2 tuiCircleGetBottomPoint2(const TuiCircle circle);
+/*!
+ * @brief Get the rightmost @ref TuiPoint2 of a @ref TuiCircle.
+ *
+ * @param circle The @ref TuiCircle.
+ *
+ * @returns The rightmost @ref TuiPoint2
+ *
+ * @errors This function can have no error codes.
+ *
+ * @requirements This function can be called freely, even if TUIC is not currently initialized.
+ *
+ * @thread_safety This function does not access memory in ways that could cause a data race, so it is thread safe.
+ */
+TuiPoint2 tuiCircleGetRightPoint2(const TuiCircle circle);
+/*!
+ * @brief Get if a @ref TuiCircle is degenerate. A degenerate @ref TuiCircle has a negative radius.
+ *
+ * @param circle The @ref TuiCircle.
+ *
+ * @returns if the @ref TuiCircle is degenerate.
+ *
+ * @errors This function can have no error codes.
+ *
+ * @requirements This function can be called freely, even if TUIC is not currently initialized.
+ *
+ * @thread_safety This function does not access memory in ways that could cause a data race, so it is thread safe.
+ */
+TuiBoolean tuiCircleIsDegenerate(const TuiCircle circle);
+/*!
+ * @brief Get if a @ref TuiCircle contains a @ref TuiPoint2.
+ *
+ * @param circle The @ref TuiCircle.
+ * @param point2 The @ref TuiPoint2
+ *
+ * @returns if the @ref TuiCircle contains the @ref TuiPoint2.
+ *
+ * @errors This function can have no error codes.
+ *
+ * @requirements This function can be called freely, even if TUIC is not currently initialized.
+ *
+ * @thread_safety This function does not access memory in ways that could cause a data race, so it is thread safe.
+ */
+TuiBoolean tuiCircleContainsPoint2(const TuiCircle circle, const TuiPoint2 point2);
+/*!
+ * @brief Get if a @ref TuiCircle contains a @ref TuiLine.
+ *
+ * @param circle The @ref TuiCircle.
+ * @param line The @ref TuiLine
+ *
+ * @returns if the @ref TuiCircle contains the @ref TuiLine.
+ *
+ * @errors This function can have no error codes.
+ *
+ * @requirements This function can be called freely, even if TUIC is not currently initialized.
+ *
+ * @thread_safety This function does not access memory in ways that could cause a data race, so it is thread safe.
+ */
+TuiBoolean tuiCircleContainsLine(const TuiCircle circle, const TuiLine line);
+/*!
+ * @brief Get if a @ref TuiCircle contains a @ref TuiRect.
+ *
+ * @param circle The @ref TuiCircle.
+ * @param rect The @ref TuiRect
+ *
+ * @returns if the @ref TuiCircle contains the @ref TuiRect.
+ *
+ * @errors This function can have no error codes.
+ *
+ * @requirements This function can be called freely, even if TUIC is not currently initialized.
+ *
+ * @thread_safety This function does not access memory in ways that could cause a data race, so it is thread safe.
+ */
+TuiBoolean tuiCircleContainsRect(const TuiCircle circle, const TuiRect rect);
+/*!
+ * @brief Get if a @ref TuiCircle contains a @ref TuiCircle.
+ *
+ * @param circle_1 The first @ref TuiCircle.
+ * @param circle_2 The second @ref TuiCircle
+ *
+ * @returns if the @ref TuiCircle contains the @ref TuiCircle.
+ *
+ * @errors This function can have no error codes.
+ *
+ * @requirements This function can be called freely, even if TUIC is not currently initialized.
+ *
+ * @thread_safety This function does not access memory in ways that could cause a data race, so it is thread safe.
+ */
+TuiBoolean tuiCircleContainsCircle(const TuiCircle circle_1, const TuiCircle circle_2);
+/*!
+ * @brief Get if a @ref TuiCircle intersects a @ref TuiLine.
+ *
+ * @param circle The @ref TuiCircle.
+ * @param line The @ref TuiLine
+ *
+ * @returns if the @ref TuiCircle intersects the @ref TuiLine.
+ *
+ * @errors This function can have no error codes.
+ *
+ * @requirements This function can be called freely, even if TUIC is not currently initialized.
+ *
+ * @thread_safety This function does not access memory in ways that could cause a data race, so it is thread safe.
+ */
+TuiBoolean tuiCircleIntersectsLine(const TuiCircle circle, const TuiLine line);
+/*!
+ * @brief Get if a @ref TuiCircle intersects a @ref TuiRect.
+ *
+ * @param circle The @ref TuiCircle.
+ * @param rect The @ref TuiRect
+ *
+ * @returns if the @ref TuiCircle intersects the @ref TuiRect.
+ *
+ * @errors This function can have no error codes.
+ *
+ * @requirements This function can be called freely, even if TUIC is not currently initialized.
+ *
+ * @thread_safety This function does not access memory in ways that could cause a data race, so it is thread safe.
+ */
+TuiBoolean tuiCircleIntersectsRect(const TuiCircle circle, const TuiRect rect);
+/*!
+ * @brief Get if a @ref TuiCircle intersects a @ref TuiCircle.
+ *
+ * @param circle_1 The first @ref TuiCircle.
+ * @param circle_2 The second @ref TuiCircle
+ *
+ * @returns if the @ref TuiCircle intersects the @ref TuiCircles.
+ *
+ * @errors This function can have no error codes.
+ *
+ * @requirements This function can be called freely, even if TUIC is not currently initialized.
+ *
+ * @thread_safety This function does not access memory in ways that could cause a data race, so it is thread safe.
+ */
+TuiBoolean tuiCircleIntersectsCircle(const TuiCircle circle_1, const TuiCircle circle_2);
+/*! @} */
 
 
 #ifdef __cplusplus //extern C guard
---
+++ src/circle.c
@@ -0,0 +1,131 @@
+/*
+	Copyright (c) 2021 Daniel Valcour
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy of
+	this software and associated documentation files (the "Software"), to deal in
+	the Software without restriction, including without limitation the rights to
+	use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+	the Software, and to permit persons to whom the Software is furnished to do so,
+	subject to the following conditions:
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+	FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+	COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+	IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+	CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+#include <TUIC/circle.h>
+#include <TUIC/point2.h>
+#include <TUIC/line.h>
+#include <TUIC/rect.h>
+#include "grid_shapes_inline.h"
+#include <math.h>
+#include <stdlib.h>
+#include <TUIC/easing.h>
+
+TuiCircle tuiCircle(const int center_x, const int center_y, const float radius)
+{
+	TuiCircle ret = { center_x, center_y, radius };
+	return ret;
+}
+
+TuiPoint2 tuiCircleGetCenterPoint2(const TuiCircle circle)
+{
+	TuiPoint2 ret = { circle.center_x, circle.center_y };
+	return ret;
+}
+
+TuiPoint2 tuiCircleGetTopPoint2(const TuiCircle circle)
+{
+	TuiPoint2 ret = { circle.center_x, (int)roundf((float)circle.center_y - circle.radius) };
+	return ret;
+}
+
+TuiPoint2 tuiCircleGetLeftPoint2(const TuiCircle circle)
+{
+	TuiPoint2 ret = { (int)roundf((float)circle.center_x - circle.radius), circle.center_y };
+	return ret;
+}
+
+TuiPoint2 tuiCircleGetBottomPoint2(const TuiCircle circle)
+{
+	TuiPoint2 ret = { circle.center_x, (int)roundf((float)circle.center_y + circle.radius) };
+	return ret;
+}
+
+TuiPoint2 tuiCircleGetRightPoint2(const TuiCircle circle)
+{
+	TuiPoint2 ret = { (int)roundf((float)circle.center_x + circle.radius), circle.center_y };
+	return ret;
+}
+
+TuiBoolean tuiCircleIsDegenerate(const TuiCircle circle)
+{
+	return (circle.radius < 0);
+}
+
+TuiBoolean tuiCircleContainsPoint2(const TuiCircle circle, const TuiPoint2 point2)
+{
+	const float circle_border_squared_distance = (float)((point2.x * point2.x) + (point2.y * point2.y)) - (circle.radius * circle.radius);
+	return circle_border_squared_distance <= 0.0f;
+}
+
+TuiBoolean tuiCircleContainsLine(const TuiCircle circle, const TuiLine line)
+{
+	return tuiCircleContainsPoint2(circle, tuiLineGetStartPoint2(line)) && tuiCircleContainsPoint2(circle, tuiLineGetEndPoint2(line));
+}
+
+TuiBoolean tuiCircleContainsRect(const TuiCircle circle, const TuiRect rect)
+{
+	return
+		tuiCircleContainsPoint2(circle, tuiRectGetTopRightInnerCornerPoint2(rect)) &&
+		tuiCircleContainsPoint2(circle, tuiRectGetTopLeftInnerCornerPoint2(rect)) &&
+		tuiCircleContainsPoint2(circle, tuiRectGetBottomRightInnerCornerPoint2(rect)) &&
+		tuiCircleContainsPoint2(circle, tuiRectGetBottomLeftInnerCornerPoint2(rect));
+}
+
+TuiBoolean tuiCircleContainsCircle(const TuiCircle circle_1, const TuiCircle circle_2)
+{
+	return
+		tuiCircleContainsPoint2(circle_1, tuiCircleGetCenterPoint2(circle_2)) &&
+		tuiCircleContainsPoint2(circle_1, tuiCircleGetLeftPoint2(circle_2)) &&
+		tuiCircleContainsPoint2(circle_1, tuiCircleGetTopPoint2(circle_2)) &&
+		tuiCircleContainsPoint2(circle_1, tuiCircleGetRightPoint2(circle_2)) &&
+		tuiCircleContainsPoint2(circle_1, tuiCircleGetBottomPoint2(circle_2));
+}
+
+TuiBoolean tuiCircleIntersectsLine(const TuiCircle circle, const TuiLine line)
+{
+	if (circle.radius <= 0) return TUI_FALSE;
+	TuiPoint2 start_point = tuiLineGetStartPoint2(line);
+	TuiPoint2 end_point = tuiLineGetEndPoint2(line);
+	if (tuiCircleContainsPoint2(circle, start_point) || tuiCircleContainsPoint2(circle, end_point)) return TUI_TRUE;
+	const float line_length = tuiLineGetLength(line);
+	const float dot_product = (((circle.center_x - line.start_x) * (line.end_x - line.start_x)) + ((circle.center_y - line.start_y) * (line.end_y - line.start_y))) / (line_length * line_length);
+	const float closest_x = roundf(line.start_x + (dot_product * (line.end_x - line.start_x)));
+	const float closest_y = roundf(line.start_y + (dot_product * (line.end_y - line.start_y)));
+	if (!_Point2OnCollinearLine(start_point, end_point, tuiPoint2((int)closest_x, (int)closest_y))) return TUI_FALSE;
+	const float distance_x = closest_x - circle.center_x;
+	const float distance_y = closest_y - circle.center_y;
+	const float distance = sqrtf((distance_x * distance_x) + (distance_y * distance_y));
+	return (distance <= circle.radius);
+}
+
+TuiBoolean tuiCircleIntersectsRect(const TuiCircle circle, const TuiRect rect)
+{
+	if (rect.width <= 0 || rect.height <= 0 || circle.radius <= 0) return TUI_FALSE;
+	const TuiPoint2 closest_rect_point = tuiPoint2(CLAMP(rect.x, rect.x + rect.width - 1, circle.center_x), CLAMP(rect.y, rect.y + rect.height - 1, circle.center_y));
+	const float point_distance = tuiPoint2Distance(tuiCircleGetCenterPoint2(circle), closest_rect_point);
+	return (point_distance <= circle.radius);
+}
+
+TuiBoolean tuiCircleIntersectsCircle(const TuiCircle circle_1, const TuiCircle circle_2)
+{
+	if (circle_1.radius <= 0 || circle_2.radius <= 0) return TUI_FALSE;
+	const float point_distance = tuiPoint2Distance(tuiCircleGetCenterPoint2(circle_1), tuiCircleGetCenterPoint2(circle_2));
+	const float combined_radius = circle_1.radius + circle_2.radius;
+	return (point_distance < combined_radius);
+}
