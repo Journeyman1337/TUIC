d313aa38c25d52c5c6ba22d1d38b71684747255a
Journeyman <60055347+Journeyman-dev@users.noreply.github.com>
2021-09-19 12:33:35 -0400

    fix tuiPoint2GetPerpDotProduct

--- src/point2.c
+++ src/point2.c
@@ -45,7 +45,7 @@ float tuiPoint2GetDistance(const TuiPoint2 point2_1, const TuiPoint2 point2_2)
 
 int tuiPoint2GetPerpDotProduct(const TuiPoint2 point2_1, const TuiPoint2 point2_2)
 {
-	return (point2_1.x * point2_2.x) - (point2_1.y * point2_2.y);
+	return (point2_1.x * point2_2.y) - (point2_2.x * point2_1.y);
 }
 
 float tuiPoint2GetDotProduct(const TuiPoint2 point2_1, const TuiPoint2 point2_2)
