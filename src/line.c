aac2120e5506491fd40f217dc09017a0fecea594
Journeyman <60055347+Journeyman-dev@users.noreply.github.com>
2021-09-19 16:56:12 -0400

    use tuiLineGetTranslationPoint2 to get the translations in tuiLinesGetDotProduct
    encapsulate

--- src/line.c
+++ src/line.c
@@ -125,10 +125,12 @@ float tuiLinesGetDotProduct(const TuiLine line_1, const TuiLine line_2)
 	const float line_1_length = tuiLineGetLength(line_1);
 	const float line_2_length = tuiLineGetLength(line_2);
 	if (line_1_length == 0.0f || line_2_length == 0.0f) return 0.0f;
-	const float line_1_unit_x = (float)(line_1.end_x - line_1.start_x) / line_1_length;
-	const float line_1_unit_y = (float)(line_1.end_y - line_1.start_y) / line_1_length;
-	const float line_2_unit_x = (float)(line_2.end_x - line_2.start_x) / line_2_length;
-	const float line_2_unit_y = (float)(line_2.end_y - line_2.start_y) / line_2_length;
+	const TuiPoint2 line_1_translation = tuiLineGetTranslationPoint2(line_1);
+	const TuiPoint2 line_2_translation = tuiLineGetTranslationPoint2(line_2);
+	const float line_1_unit_x = (float)(line_1_translation.x) / line_1_length;
+	const float line_1_unit_y = (float)(line_1_translation.y) / line_1_length;
+	const float line_2_unit_x = (float)(line_2_translation.x) / line_2_length;
+	const float line_2_unit_y = (float)(line_2_translation.y) / line_2_length;
 	return (line_1_unit_x * line_2_unit_x) + (line_1_unit_y * line_2_unit_y);
 }
 
