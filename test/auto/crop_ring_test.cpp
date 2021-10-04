5c40b50c0b701e86a5f1ac346886a4a9744bcd44
Journeyman <60055347+Journeyman-dev@users.noreply.github.com>
2021-10-04 16:39:52 -0400

    fis tui crop ring test case names

--- test/auto/crop_ring_test.cpp
+++ test/auto/crop_ring_test.cpp
@@ -3,152 +3,192 @@
 #include <catch2/catch.hpp>
 
 
-TEST_CASE("tuiRingGetCenterPoint2")
+TEST_CASE("tuiCropRingGetRingCenterPoint2")
 {
 
 }
 
-TEST_CASE("tuiRingGetLeftX")
+TEST_CASE("tuiCropRingGetRingLeftX")
 {
 
 }
 
-TEST_CASE("tuiRingGetRightX")
+TEST_CASE("tuiCropRingGetRingRightX")
 {
 
 }
 
-TEST_CASE("tuiRingGetTopY")
+TEST_CASE("tuiCropRingGetRingTopY")
 {
 
 }
 
-TEST_CASE("tuiRingGetBottomY")
+TEST_CASE("tuiCropRingGetRingBottomY")
 {
 
 }
 
-TEST_CASE("tuiRingGetInnerRadius")
+TEST_CASE("tuiCropRingGetInnerRingRadius")
 {
 
 }
 
-TEST_CASE("tuiRingGetInnerLeftX")
+TEST_CASE("tuiCropRingGetInnerRingLeftX")
 {
 
 }
 
-TEST_CASE("tuiRingGetInnerRightX")
+TEST_CASE("tuiCropRingGetInnerRingRightX")
 {
 
 }
 
-TEST_CASE("tuiRingGetInnerTopY")
+TEST_CASE("tuiCropRingGetInnerRingTopY")
 {
 
 }
 
-TEST_CASE("tuiRingGetInnerBottomY")
+TEST_CASE("tuiCropRingGetInnerRingBottomY")
 {
 
 }
 
-TEST_CASE("tuiRingGetDimensions")
+TEST_CASE("tuiCropRingGetRingDimensions")
 {
 
 }
 
-TEST_CASE("tuiRingGetInnerDimensions")
+TEST_CASE("tuiCropRingGetInnerRingDimensions")
 {
 
 }
 
-TEST_CASE("tuiRingGetCircle")
+TEST_CASE("tuiCropRingGetCropFarX")
 {
 
 }
 
-TEST_CASE("tuiRingGetInnerCircle")
+TEST_CASE("tuiCropRingGetCropFarY")
 {
 
 }
 
-TEST_CASE("tuiRingGetBoundingRect")
+TEST_CASE("tuiCropRingGetLeftX")
 {
 
 }
 
-TEST_CASE("tuiRingGetInnerBoundingRect")
+TEST_CASE("tuiCropRingGetRightX")
 {
 
 }
 
-TEST_CASE("tuiRingGetLeftPoint2")
+TEST_CASE("tuiCropRingGetTopY")
 {
 
 }
 
-TEST_CASE("tuiRingGetRightPoint2")
+TEST_CASE("tuiCropRingGetBottomY")
 {
 
 }
 
-TEST_CASE("tuiRingGetTopPoint2")
+TEST_CASE("tuiCropRingGetRing")
 {
 
 }
 
-TEST_CASE("tuiRingGetBottomPoint2")
+TEST_CASE("tuiCropRingGetRingCircle")
 {
 
 }
 
-TEST_CASE("tuiRingGetInnerLeftPoint2")
+TEST_CASE("tuiCropRingGetRingInnerCircle")
 {
 
 }
 
-TEST_CASE("tuiRingGetInnerRightPoint2")
+TEST_CASE("tuiCropRingGetRingBoundingRect")
 {
 
 }
 
-TEST_CASE("tuiRingGetInnerTopPoint2")
+TEST_CASE("tuiCropRingGetInnerRingBoundingRect")
 {
 
 }
 
-TEST_CASE("tuiRingGetInnerBottomPoint2")
+TEST_CASE("tuiCropRingGetBoundingRect")
 {
 
 }
 
-TEST_CASE("tuiRingIsDegenerate")
+TEST_CASE("tuiCropRingGetRingLeftPoint2")
 {
 
 }
 
-TEST_CASE("tuiRingContainsPoint2")
+TEST_CASE("tuiCropRingGetRingRightPoint2")
 {
 
 }
 
-TEST_CASE("tuiRingIntersetsLine")
+TEST_CASE("tuiCropRingGetRingTopPoint2")
 {
 
 }
 
-TEST_CASE("tuiRingIntersectsRect")
+TEST_CASE("tuiCropRingGetRingBottomPoint2")
 {
 
 }
 
-TEST_CASE("tuiRingIntersectsCircle")
+TEST_CASE("tuiCropRingGetRingInnerLeftPoint2")
 {
 
 }
 
-TEST_CASE("tuiRingIntersectsRing")
+TEST_CASE("tuiCropRingGetRingInnerRightPoint2")
+{
+
+}
+
+TEST_CASE("tuiCropRingGetRingInnerBottomPoint2")
+{
+
+}
+
+TEST_CASE("tuiCropRingIsDegenerate")
+{
+
+}
+
+TEST_CASE("tuiCropRingContainsPoint2")
+{
+
+}
+
+TEST_CASE("tuiCropRingIntersetsLine")
+{
+
+}
+
+TEST_CASE("tuiCropRingIntersectsRect")
+{
+
+}
+
+TEST_CASE("tuiCropRingIntersectsCircle")
+{
+
+}
+
+TEST_CASE("tuiCropRingIntersectsRing")
+{
+
+}
+
+TEST_CASE("tuiCropRingIntersectsCropRing")
 {
 
 }
